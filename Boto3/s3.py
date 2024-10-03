2afdac80dda55d67a4b68db1cb01b7c5423b74cc
heloise <37453877+heloise-viegas@users.noreply.github.com>
2024-10-03 22:36:47 +0530

    Update s3.py, fixed erros in json
    error parsing iam resource arn line 23

--- Boto3/s3.py
+++ Boto3/s3.py
@@ -1,22 +1,33 @@
 import boto3
-client = boto3.client('s3')
-response = client.list_buckets()
+import json
+
+
+session = boto3.Session(profile_name='Certification')
+s3=session.client('s3')
+response = s3.list_buckets()
+#print(response)
 fullaccess_policy={}
 
-for s3 in response['Buckets']:
-    print(f"--{s3['Name']}")
+for s3bucket in response['Buckets']:
+    #print(f" S3 Bucket: {s3bucket['Name']}")
     try:
-        policy = client.get_bucket_policy(
-        Bucket=s3['Name']
-        )
-        actions=policy['Policy']['Statement'][0]['Action']
-        for action in actions:
-            if '*' in action:
-                fullaccess_policy[s3['Name']]=policy['Policy']['Id']
-    except client.exceptions.NoSuchBucketPolicy:
-        print(f"No policy found for bucket {s3['Name']}")
-for i,n in fullaccess_policy().items:
-    print(f"{i}:{n}")
+        policy = s3.get_bucket_policy(
+        Bucket=s3bucket['Name']      )
+        #print(f"Policy:{policy}")
+        #print(policy['Policy']) ## it has ' quotes hence its considere  as a string hence the  error:TypeError: string indices must be integers
+        json_object = json.loads(policy['Policy'])  ## converts json string to json obj
+        for stmt in json_object['Statement']:
+            #print(stmt['Action'])
+            if '*' in stmt['Action']:
+                print(stmt)
+                obj=json.loads(stmt['Resource'])
+                print(obj)
+                fullaccess_policy[s3bucket['Name']]=stmt['Sid']
+    except s3.exceptions.from_code('NoSuchBucketPolicy'):
+        print(f"No policy found for bucket {s3bucket['Name']}")
+#         continue
+# for i,n in fullaccess_policy().items:
+#     print(f"{i}:{n}")
   
 
 
