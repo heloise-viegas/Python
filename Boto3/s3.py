89ceeea86256ea4188d92529adde9caa544c3c29
heloise <37453877+heloise-viegas@users.noreply.github.com>
2024-10-05 16:14:16 +0530

    ci: deleted unwanted code

--- Boto3/s3.py
+++ Boto3/s3.py
@@ -1,63 +1,50 @@
 import boto3
 import json
 
-
+# Initialize a session using a specific profile
 session = boto3.Session(profile_name='Certification')
-s3=session.client('s3')
+s3 = session.client('s3')
+
+# List all S3 buckets
 response = s3.list_buckets()
-#print(response)
-fullaccess_policy={}
-public_access_s3={}
+
+# Dictionaries to store policies and access information
+fullaccess_policy = {}
+nobucket_policy = {}
+public_access_s3 = {}
+except_public_access_s3 = {}
 
 def check_s3_policy(s3bucket):
     try:
-        policy = s3.get_bucket_policy(
-        Bucket=s3bucket['Name']      )
-        ##print(policy['Policy']) ## it has ' quotes hence its considere  as a string hence the  error:TypeError: string indices must be integers
-        json_object = json.loads(policy['Policy'])  ## converts json string to json obj
+        policy = s3.get_bucket_policy(Bucket=s3bucket['Name'])
+        json_object = json.loads(policy['Policy'])  # Convert JSON string to JSON object
         for stmt in json_object['Statement']:
-            ##print(stmt['Action'])
             if '*' in stmt['Action']:
-                ## print(stmt)
-                ## obj=stmt['Resource'][0]
-                ## print(obj)
-                ## fullaccess_policy[s3bucket['Name']]=stmt['Sid']
-                fullaccess_policy[s3bucket['Name']]="A"
+                fullaccess_policy[s3bucket['Name']] = "A"  # Placeholder for full access policy
     except s3.exceptions.from_code('NoSuchBucketPolicy'):
-        #print(f"No policy found for bucket {s3bucket['Name']}")
-        fullaccess_policy[s3bucket['Name']]='NoSuchBucketPolicy'
+        nobucket_policy[s3bucket['Name']] = 'NoSuchBucketPolicy'
 
 def check_s3_publicaccess(s3bucket):
     try:
-        public_access=s3.get_public_access_block(
-        Bucket=s3bucket['Name']  )
-        if (public_access['PublicAccessBlockConfiguration']['BlockPublicAcls']) == False:
-            public_access_s3[s3bucket['Name']]="Public "
-
-    # except Exception as e:
-    #     print(f"{s3bucket['Name']}: {e}")
+        public_access = s3.get_public_access_block(Bucket=s3bucket['Name'])
+        if not public_access['PublicAccessBlockConfiguration']['BlockPublicAcls']:
+            public_access_s3[s3bucket['Name']] = "Public"
     except s3.exceptions.from_code('NoSuchPublicAccessBlockConfiguration'):
-        #print(f"NoSuchPublicAccessBlockConfiguration: The public access block configuration was not found for {s3bucket['Name']}")
-        public_access_s3[s3bucket['Name']]='NoSuchPublicAccessBlockConfiguration'
+        except_public_access_s3[s3bucket['Name']] = 'NoSuchPublicAccessBlockConfiguration'
     except s3.exceptions.from_code('AccessDenied'):
-        print(f"AccessDenied: User: arn:aws:sts::503694153160:assumed-role/AWSReservedSSO_Admin_Access_To_Limited_Regions_fe80549b72368bef/heloise.viegas@creativecapsule.com is not authorized to perform: s3:GetBucketPublicAccessBlock on resource: {s3bucket['Name']}")
-        public_access_s3[s3bucket['Name']]='AccessDenied'
+        except_public_access_s3[s3bucket['Name']] = 'AccessDenied'
 
-
+# Iterate over each S3 bucket and check policies and public access
 for s3bucket in response['Buckets']:
-################check full access permission###################################
     check_s3_policy(s3bucket)
-################check public access###################################
     check_s3_publicaccess(s3bucket)
 
-###print s3 with full access
-for i in fullaccess_policy:
-    print(f"S3 buckets with Full access: {i}")
+# Print S3 buckets with full access
+print("S3 buckets with Full access:")
+for item in fullaccess_policy:
+    print(f"{item}: {fullaccess_policy[item]}")
 
-###print s3 with public access
-for i in public_access_s3:
-    print(f"S3 buckets with Public access: {i}")
-
-
-
-
+# Print S3 buckets with public access
+print("S3 buckets with Public access:")
+for item in public_access_s3:
+    print(f"{item}: {public_access_s3[item]}")
